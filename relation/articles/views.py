6042297f47742482af98350b0702e9e29ce2266c
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-03-19 17:19:47 +0900

    update

--- relation/articles/views.py
+++ relation/articles/views.py
@@ -5,13 +5,18 @@ from .serializers import ArticleSerializer, CommentSerializer, Comment
 from accounts.serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import logging
+
+logger = logging.getLogger(__name__)
 
 @api_view(["GET", "POST"])
 def article_list(request):
     if request.method == "GET":
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        # return Response(serializer.data)
+
+        logger.info("articles")
+
         return Response(serializer.data)
 
     elif request.method == "POST":
