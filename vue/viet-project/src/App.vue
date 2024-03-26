e6b2795c3ad1a32c53da7efcb014de0e858a1bce
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-03-26 17:36:44 +0900

    vue folder update

---
+++ vue/viet-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 :class="myClass">Hello, {{ myClass }}</h1>
+  </div>
+</template>
+
+<script setup>
+
+const myText = 'Vue 3'
+const myClass = ['bold', 'color', 'size']
+
+</script>
+
+<style lang="scss" scoped>
+.bold {
+  font-weight: bold;
+}
+.color {
+  color: blueviolet;
+}
+.size {
+  font-size: 5rem;
+}
+</style>
